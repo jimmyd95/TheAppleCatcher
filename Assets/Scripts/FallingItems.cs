fbb7207b621a1d445dae11a21fc3a6fe905c1709
jimmyd95 <jimmyding2014@gmail.com>
2023-04-24 22:36:48 -0400

    Implemented the apple and the falling script
    Got the dog asset as the apple that will fall from the sky. It's cute alright. No one actually want to catch apples, they like dogs so they will catch them... And yes, random generated location so dogs will fall at random speed and random location from the sky

---
+++ Assets/Scripts/FallingItems.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingItems : MonoBehaviour
+{
+    [SerializeField] private GameObject _item;
+    [SerializeField] private float _gravity = 1;
+
+    // Spawn object from the ceiling at random location
+    private void SpawnObject()
+    {
+        var targetRigidbody = _item.GetComponent<Rigidbody>();
+        // set random falling location x and z, y is fixed because we need the height to be at the same position
+        float _x = Random.Range(0, 100);
+        float _y = 5;
+        float _z = Random.Range(0, 100);
+        Vector3 temp = new Vector3(_x, _y, _z);
+        // add force for creating falling speed
+        targetRigidbody.AddForce(Physics.gravity * targetRigidbody.mass * _gravity);
+        Instantiate(_item, transform.position, Quaternion.identity); // and then pop the item!
+    }
+}
