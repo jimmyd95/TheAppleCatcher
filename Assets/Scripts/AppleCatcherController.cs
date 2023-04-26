9b23708c455f6419f0ea084d81d9d1c735ee8d2e
jimmyd95 <jimmyding2014@gmail.com>
2023-04-26 00:43:30 -0400

    APK build
    The apk built for android works, but then nothing else does... along with the menu screen, which is insanely annoying...

--- Assets/Scripts/AppleCatcherController.cs
+++ Assets/Scripts/AppleCatcherController.cs
@@ -7,12 +7,11 @@ public class AppleCatcherController : MonoBehaviour
     public bool _paused = false; // set the game to 
 
     [SerializeField] private GameObject _appleGeneratorField;
-    [SerializeField] private int _fallingAppleAmount = 100;
+    [SerializeField] private int _fallingAppleAmount = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -21,22 +20,15 @@ public class AppleCatcherController : MonoBehaviour
         StopTime();
     }
 
-    public void GameStart()
+    public void DroppingApples()
     {
-        // generate x amount of apples when called this function
-        for (int i = 0; i < _fallingAppleAmount; i++)
-        {
-            //float time = Random.Range(0, 100); // set a random wait time for each apple to fall
-            //WaitForCertainPeriod(time); // wait for the time
-            _appleGeneratorField.GetComponent<FallingItems>().SpawnObject(); // spawn the apple
-        }
+        float temp = Random.Range(0, 3);
+        Invoke(nameof(GenerateApple), temp);
     }
 
-    // Set a wait time
-    public IEnumerator WaitForCertainPeriod(float time)
+    void GenerateApple()
     {
-        Debug.Log("Dropping in " + time + " seconds");
-        yield return new WaitForSeconds(time);
+        _appleGeneratorField.GetComponent<FallingItems>().SpawnObject(); // spawn the apple
     }
 
     // stop time depends on if pause has been triggered
--- Assets/Scripts/FallingItems.cs
+++ Assets/Scripts/FallingItems.cs
@@ -12,13 +12,14 @@ public class FallingItems : MonoBehaviour
     {
         var targetRigidbody = _item.GetComponent<Rigidbody>();
         // set random falling location x and z, y is fixed because we need the height to be at the same position
-        float _x = Random.Range(0, 10);
-        float _y = _item.transform.position.y;
-        float _z = Random.Range(0, 10);
+        float _x = Random.Range(0, 5);
+        float _y = transform.position.y;
+        float _z = Random.Range(0, 5);
         Vector3 temp = new Vector3(_x, _y, _z);
+        Debug.Log(temp);
         // add force for creating falling speed
-        targetRigidbody.AddForce(new Vector3(0, -1, 0) * _gravity);
-        Instantiate(_item, transform.position, Quaternion.identity); // and then pop the item!
+        //targetRigidbody.AddForce(new Vector3(0, -1, 0) * _gravity);
+        Instantiate(_item, temp, Quaternion.identity); // and then pop the item!
     }
 
     private void OnDestroy()
